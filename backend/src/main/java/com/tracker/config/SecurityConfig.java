09269b972ac88e4de2a8b99e566813d46984be93
Pradeepkumar <dhonipradeep1811@gmail.com>
2026-07-15 10:57:33 +0530

    cors fix

--- backend/src/main/java/com/tracker/config/SecurityConfig.java
+++ backend/src/main/java/com/tracker/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://expense-tracker-pi-blue.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
